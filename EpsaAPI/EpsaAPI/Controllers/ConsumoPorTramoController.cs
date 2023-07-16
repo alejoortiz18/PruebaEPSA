bc04d1892e48078a4466f2159d7f20898e35912c
alejoortiz18 <aortizgaviria@gmail.com>
2023-07-16 17:08:00 -0500

    Se agrega archivo de texto
    Se agrega archivo de texto al proyecto con las tareas solicitadas por el cliente

--- EpsaAPI/EpsaAPI/Controllers/ConsumoPorTramoController.cs
+++ EpsaAPI/EpsaAPI/Controllers/ConsumoPorTramoController.cs
@@ -23,7 +23,27 @@ namespace EpsaAPI.Controllers
         #endregion
 
         #region METODOS PARA CONSULTAR 
-
+        /// <summary>
+        /// <br/>Control de Cambios-----------------
+        /// <br/>EJERCICIO:    Request 1
+        /// <br/> PLANTEAMIENTO: Se envía una fecha inicial y una final (yyyy-MM-dd) y se debe de responder con una historia por cada tramo, que contenga el consumo, 
+        /// <br/>perdidas y costo por el consumo.Esto permite dar una contextualización al cliente de como han estado los datos en ese periodo de tiempo.
+        /// <br/>Creado:         Julio. 16/2023
+        /// <br/>Modificado:     
+        /// <br/>Version:        1.0
+        /// <br/>Autor:          RAOG(mi empresa) - René Alejandro Ortiz Gaviria
+        /// <br/>Sistema:        EpsaAPI Bussines
+        /// <br/>Assemblies:     EpsaAPI.EpsaBLL.Bussines
+        /// <br/>Description:    Metodo para Obtener Historial de consumo de todos los tramos filtrado por fecha inicial y fecha final
+        /// <br/><param name="fecha">Param: objeto FechasDto</param>
+        /// <br/><returns>Retorna: List ObtenerHistoriaConsumoDto</returns>
+        /// <br/><exception cref="Exception">Exception: se ejecuta un throw en caso de tener alguna excepción </exception>
+        /// <br/> Historial--------------------------
+        /// <br/>Julio. 16/2023 - RAOG Version: 1.0. Creación Clase ConsumoPorTramoDAL
+        /// <br/><Author>
+        /// <br/>René Alejandro Ortiz Gaviria
+        /// <br/></Author>
+        /// <br/></summary>
         [ResponseType(typeof(ObtenerHistoriaConsumoDto))]
         [Route("ConsumoPorTramo")]
         public IHttpActionResult ConsumoPorTramo(FechasDto date)
